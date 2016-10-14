a1fed4b96e7e14e5fa72da3630172c17d20696ed
swickaphil <phillip.swickard@anthem.com>
2016-10-13 23:19:34 -0700

    added projects

--- js/main.js
+++ js/main.js
@@ -11,17 +11,19 @@ $( document ).ready(function() {
       projectList = data.projects;
       var injectString = '';
       for (var i = 0; i < projectList.length; i++) {
-        console.log(i % 3);
-        if (i % 3 == 0) {
+        console.log(i % 4);
+        if (i % 4 == 0) {
           injectString += '<div class="row row-projects">'
         }
-        injectString += '<div id="' + i + '" class="col-md-4 project">'
-        + '<img src="' + projectList[i].thumb + '" class="img-responsive" alt="leadership image">'
-        + '<h2>' + projectList[i].title + '</h2>'
+        injectString += '<div id="' + i + '" class="col-md-3 project">'
+        + '<div class="img-responsive project-overlay">'
+        + '</div>'
+        + '<img src="' + projectList[i].thumb + '" class="img-responsive thumbnail" alt="' + projectList[i].title + ' project">'
+        + '<h4>' + projectList[i].title + '</h4>'
         + '<p>' + projectList[i].subtitle + '</p>'
         + '</div>';
 
-        if (i % 3 == 2 || i == projectList.length - 1){
+        if (i % 4 == 3 || i == projectList.length - 1){
           injectString += '</div>';
           if(i == projectList.length - 1){
             injectString += "<div class='btn-container'><a href='#connect' class='btn btn-default btn-lg btn-connect'>Let's Connect</a></div>";
--- js/project.js
+++ js/project.js
@@ -7,5 +7,55 @@ $( document ).ready(function() {
   console.log(projectDetailsObj);
 
   $(".title").text(projectDetailsObj.title)
+  
+  // $(".thumb").attr("src", projectDetailsObj.thumb)
 
+  /*for (var i = 0; i < projectDetailsObj.images.length; i++) {
+    $(".carousel-indicators").append('<li data-target="#carousel-example-generic" data-slide-to="' + i + '" class="active"></li>');
+    
+    var imgHTML = '';
+
+    if (i == 0) {
+      imgHTML = '<div class="item active">'
+              + '<img src="' + projectDetailsObj.images[i] + '" alt="...">'
+              + '<div class="carousel-caption">'
+              +  '...'
+              + '</div>'
+            '</div>'
+    }else{
+      imgHTML + '<div class="item">'
+              + '<img src="..." alt="...">'
+              + '<div class="carousel-caption">'
+              +  '...'
+              + '</div>'
+            '</div>'
+    }
+    
+    $(".carousel-inner").append(imgHTML);
+
+  }*/
+
+
+
+  for (var i = 0; i < projectDetailsObj.links.length; i++) {
+    $(".links").append("<a href='" + projectDetailsObj.links[i] + "'>LINK</a>");
+    if (i != projectDetailsObj.links.length - 1) {
+      $(".links").append(", ");
+    }
+  }
+
+  $(".summary").text(projectDetailsObj.summary)
+  for (var i = 0; i < projectDetailsObj.roles.length; i++) {
+    $(".roles").append(projectDetailsObj.roles[i]);
+    if (i != projectDetailsObj.roles.length - 1) {
+      $(".roles").append(", ");
+    }
+  }
+
+  for (var i = 0; i < projectDetailsObj.tech.length; i++) {
+    $(".tech").append(projectDetailsObj.tech[i]);
+    if (i != projectDetailsObj.tech.length - 1) {
+      $(".tech").append(", ");
+    }
+  }
 });
